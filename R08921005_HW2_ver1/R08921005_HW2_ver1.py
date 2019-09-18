8a837394661f61ee1fb16969bc22c779a18b8b68
vincent0628 <vincent0910628007@gmail.com>
2019-09-18 11:45:58 +0800

    HW2 part(b) finish

--- R08921005_HW2_ver1/R08921005_HW2_ver1.py
+++ R08921005_HW2_ver1/R08921005_HW2_ver1.py
@@ -6,11 +6,14 @@ Created on Wed Sep 11 19:33:55 2019
 """
 from PIL import Image
 import numpy as np
+import pandas as pd
+import os
+
 
 lena_pic = Image.open( "lena.bmp" )
 original_lena_array = np.array(lena_pic)
-###########################################################
-#(a)	a binary image (threshold at 128)
+
+#(a)    a binary image (threshold at 128)
 binarize_lena= original_lena_array.copy()
 for ii in range(len(original_lena_array)):
     for jj in range(len(original_lena_array[ii])):
@@ -19,3 +22,20 @@ for ii in range(len(original_lena_array)):
         else:
             binarize_lena[ii][jj]=0
 Image.fromarray(binarize_lena.astype(np.uint8)).save('binarize_lena.bmp')
+
+#(b)    a histogram
+histogram = np.zeros(256)
+for row in original_lena_array:
+    for pixel in row:
+        histogram[pixel] += 1
+        
+df = pd.DataFrame(histogram,columns=['value'])
+df.to_csv('histogram.csv')
+ax = df.plot.bar(y='value', rot=0)
+ticks = ax.xaxis.get_ticklocs()
+ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
+ax.xaxis.set_ticks(ticks[::50])
+ax.xaxis.set_ticklabels(ticklabels[::50])
+fig = ax.get_figure()
+fig.savefig("histogram.png")
+
