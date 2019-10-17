8d60307a9ee17304d600d446417f5efb40891a5a
vincent <vincent0910628007@gmail.com>
2019-10-17 22:27:21 +0800

    initialize  HW4

---
+++ R08921005_HW4_ver1/HW4a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 20:56:19 2019
+
+@author: vincent黃國郡
+"""
+from PIL import Image
+import numpy as np
+import MyMorphology 
+
+if __name__ == '__main__':
+lena_pic = Image.open("lena.bmp")
+
+
---
+++ R08921005_HW4_ver1/MyMorphology.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 20:56:19 2019
+
+@author: vincent黃國郡
+"""
+from PIL import Image
+import numpy as np
+
+if __name__ == '__main__':
+
+
+
