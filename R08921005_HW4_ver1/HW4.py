424aeb0d0482604f2ad6a711782aebd2150bda01
vincent0628 <vincent0910628007@gmail.com>
2019-10-19 13:45:17 +0800

    dilation finish

---
+++ R08921005_HW4_ver1/HW4.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 20:56:19 2019
+
+@author: vincent黃國郡
+"""
+from PIL import Image
+import numpy as np
+import myMorphology 
+kernel = np.array([\
+        [0, 1, 1, 1, 0], \
+        [1, 1, 1, 1, 1], \
+        [1, 1, 1, 1, 1], \
+        [1, 1, 1, 1, 1], \
+        [0, 1, 1, 1, 0]])
+
+if __name__ == '__main__':
+    lena = Image.open("lena.bmp")
+    #binarization
+    binary_lena = lena.point(lambda x: 0 if x<128 else 255, '1')
+    
+    #HW4.1'
+    dilation_lena = myMorphology.dilation(binary_lena, kernel)
+    dilation_lena.save('dilation_lena.bmp')
+
--- R08921005_HW4_ver1/MyMorphology.py
+++ R08921005_HW4_ver1/MyMorphology.py
@@ -6,8 +6,24 @@ Created on Thu Oct 17 20:56:19 2019
 """
 from PIL import Image
 import numpy as np
-
-if __name__ == '__main__':
-
-
-
+def dilation(originalImage, kernel):
+    """
+    :type originalImage: Image 
+    :type kernel: numpy array
+    :return type: Image 
+    """
+    centerKernel = tuple([ x//2 for x in kernel.shape])
+    dilationImage = Image.new('1', originalImage.size)
+    for r in range(originalImage.size[0]):
+        for c in range(originalImage.size[1]):
+            originalPixel = originalImage.getpixel((r, c))
+            # If this pixel is 1 not white
+            if(originalPixel!=0):
+                for ii in range(kernel.shape[0]):
+                    for jj in range(kernel.shape[1]):
+                        if(kernel[ii,jj]):
+                            targetX = r + (ii - centerKernel[0])
+                            targetY = c + (jj - centerKernel[1])
+                            if ((0<targetX<originalImage.size[0])and(0<targetY<originalImage.size[1])):
+                                dilationImage.putpixel((targetX, targetY), 1)
+    return dilationImage
