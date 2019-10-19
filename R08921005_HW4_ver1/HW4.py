5bf461bf493ebf48232e03e113919e02d39c72fe
vincent0628 <vincnet0910628007@gmail.com>
2019-10-19 19:43:00 +0800

    HW4 finish

--- R08921005_HW4_ver1/HW4.py
+++ R08921005_HW4_ver1/HW4.py
@@ -7,22 +7,46 @@ Created on Thu Oct 17 20:56:19 2019
 from PIL import Image
 import numpy as np
 import myMorphology
-kernel = np.array([\
-        [0, 1, 1, 1, 0], \
-        [1, 1, 1, 1, 1], \
-        [1, 1, 1, 1, 1], \
-        [1, 1, 1, 1, 1], \
-        [0, 1, 1, 1, 0]])
+kernel = np.array([
+    [0, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 0]])
+centerKernel = tuple([x // 2 for x in kernel.shape])
 
 if __name__ == '__main__':
     lena = Image.open("lena.bmp")
-    #binarization
+    # binarization
     binary_lena = lena.point(lambda x: 0 if x < 128 else 255, '1')
 
-    #HW4.1'
-    dilation_lena = myMorphology.dilation(binary_lena, kernel)
+    # HW4(a)
+    dilation_lena = myMorphology.dilation(binary_lena, kernel, centerKernel)
     dilation_lena.save('dilation_lena.bmp')
 
-    #HW4.2'
-    erosion_lena = myMorphology.erosion(binary_lena, kernel)
+    # HW4(b)
+    erosion_lena = myMorphology.erosion(binary_lena, kernel, centerKernel)
     erosion_lena.save('erosion_lena.bmp')
+
+    # HW4(c)
+    opening_lena = myMorphology.opening(binary_lena, kernel, centerKernel)
+    opening_lena.save('opening_lena.bmp')
+
+    # HW4(d)
+    closing_lena = myMorphology.closing(binary_lena, kernel, centerKernel)
+    closing_lena.save('closing_lena.bmp')
+
+    # HW4(e)
+    kernel_J = np.array([
+        [1, 1],
+        [0, 1]])
+    centerKernel_J = (1, 0)
+    kernel_K = np.array([
+        [1, 1],
+        [0, 1]])
+    centerKernel_K = (0, 1)
+    hitmiss_lena = myMorphology.hitmiss(binary_lena,
+                                       kernel_J, centerKernel_J,
+                                       kernel_K, centerKernel_K)
+    # Save image fo file.
+    hitmiss_lena.save('hitmiss_lena.bmp')
--- R08921005_HW4_ver1/MyMorphology.py
+++ R08921005_HW4_ver1/MyMorphology.py
@@ -4,17 +4,13 @@ Created on Thu Oct 17 20:56:19 2019
 
 @author: vincent黃國郡
 """
+#    IO input type: Image
+#    except kernel: numpy array, centerKernel: tuple
+
 from PIL import Image
-import numpy as np
 
 
-def dilation(originalImage, kernel):
-    """
-    :type originalImage: Image 
-    :type kernel: numpy array
-    :return type: Image 
-    """
-    centerKernel = tuple([x // 2 for x in kernel.shape])
+def dilation(originalImage, kernel, centerKernel):
     dilationImage = Image.new('1', originalImage.size)
     for r in range(originalImage.size[0]):
         for c in range(originalImage.size[1]):
@@ -32,13 +28,7 @@ def dilation(originalImage, kernel):
     return dilationImage
 
 
-def erosion(originalImage, kernel):
-    """
-    :type originalImage: Image 
-    :type kernel: numpy array
-    :return type: Image 
-    """
-    centerKernel = tuple([x // 2 for x in kernel.shape])
+def erosion(originalImage, kernel, centerKernel):
     erosionImage = Image.new('1', originalImage.size)
     for r in range(originalImage.size[0]):
         for c in range(originalImage.size[1]):
@@ -53,7 +43,66 @@ def erosion(originalImage, kernel):
                             if (originalImage.getpixel(
                                 (targetX, targetY)) == 0):
                                 matchFlag = False
+                                break
+                        else:
+                            matchFlag = False
+                            break
             if (matchFlag):
                 erosionImage.putpixel((r, c), 1)
-
     return erosionImage
+
+
+def opening(originalImage, kernel, centerKernel):
+    return dilation(erosion(originalImage, kernel, centerKernel), kernel, centerKernel)
+
+
+def closing(originalImage, kernel, centerKernel):
+    return erosion(dilation(originalImage, kernel, centerKernel), kernel, centerKernel)
+
+def complement(originalImage):
+    complementImage = Image.new('1', originalImage.size)
+    for r in range(originalImage.size[0]):
+        for c in range(originalImage.size[1]):
+            if (originalImage.getpixel((r, c)) == 0):
+                complementImage.putpixel((r, c), 1)
+            else:
+                complementImage.putpixel((r, c), 0)
+    return  complementImage
+
+def intersection(image1, image2):
+    intersectionImage = Image.new('1', image1.size)
+    for r in range(image1.size[0]):
+        for c in range(image1.size[1]):
+            image1Pixel = image1.getpixel((r, c))
+            image2Pixel = image2.getpixel((r, c))
+            if (image1Pixel and image2Pixel):
+                intersectionImage.putpixel((r, c), 1)
+            else:
+                intersectionImage.putpixel((r, c), 0)
+    return intersectionImage
+
+def erosionWithCenter(originalImage, kernel, centerKernel):
+    erosionImage = Image.new('1', originalImage.size)
+    for r in range(originalImage.size[0]):
+        for c in range(originalImage.size[1]):
+            matchFlag = True
+            for x in range(kernel.shape[0]):
+                for y in range(kernel.shape[1]):
+                    if (kernel[x, y] == 1):
+                        destX = r + (x - centerKernel[0])
+                        destY = c + (y - centerKernel[1])
+                        if ((0 <= destX < originalImage.size[0]) and \
+                            (0 <= destY < originalImage.size[1])):
+                            if (originalImage.getpixel((destX, destY)) == 0):
+                                matchFlag = False
+                                break
+                        else:
+                            matchFlag = False
+                            break
+            if (matchFlag):
+                erosionImage.putpixel((r, c), 1)
+    return erosionImage
+
+def hitmiss(originalImage, kernel_J, centerKernel_J, kernel_K, centerKernel_K):
+    return  intersection(erosion(originalImage, kernel_J, centerKernel_J),
+                         erosion(complement(originalImage), kernel_K, centerKernel_K))
